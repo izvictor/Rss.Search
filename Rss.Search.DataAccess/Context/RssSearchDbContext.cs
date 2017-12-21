d662fa005b9b4117dfe651060d07a1327eb564a9
Victor Izverschi <victor.izverschi@outlook.com>
2017-12-21 15:19:21 +0100

    Working on authentication

--- Rss.Search.DataAccess/Context/RssSearchDbContext.cs
+++ Rss.Search.DataAccess/Context/RssSearchDbContext.cs
@@ -7,8 +7,6 @@ namespace Rss.Search.DataAccess.Context
 {
     public class RssSearchDbContext : IdentityDbContext<ApplicationUser, IdentityRole<int>, int>
     {
-        public DbSet<ApplicationUser> Users { get; set; }
-
         public RssSearchDbContext(DbContextOptions<RssSearchDbContext> options) : base(options)
         {
 
